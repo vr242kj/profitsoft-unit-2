98a053573c2a42d32ca52bcda03887c978a3c6bd
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2024-07-02 18:58:07 +0100

    added certain allowedOrigins

--- src/main/java/com/example/jsontoxml2/config/CorsConfig.java
+++ src/main/java/com/example/jsontoxml2/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:1003")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
