af778492cbd31f2ffb99af36bd0d36eb41ba7d5a
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2024-07-02 18:42:25 +0100

    - added cors police config - added buildspec.yml

--- src/main/java/com/example/jsontoxml2/controller/UserController.java
+++ src/main/java/com/example/jsontoxml2/controller/UserController.java
@@ -6,6 +6,7 @@ import com.example.jsontoxml2.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
