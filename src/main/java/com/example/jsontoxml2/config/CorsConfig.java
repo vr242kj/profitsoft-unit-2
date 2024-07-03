c6c800d485ea5d4e97dee095bdb568b4beb7dc13
Vitaliy Replyuk <vitaliy.replyuk@student.tuke.sk>
2024-07-03 17:51:40 +0100

    added frontend.uri param

--- src/main/java/com/example/jsontoxml2/config/CorsConfig.java
+++ src/main/java/com/example/jsontoxml2/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.example.jsontoxml2.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -8,13 +9,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
 
+    @Value("${frontend.uri}")
+    private String frontendUri;
+
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:1003")
+                        .allowedOrigins(frontendUri)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
